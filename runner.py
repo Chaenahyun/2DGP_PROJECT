6dbbb1f6a5c884ab08c13fcd16af2288e88ac6a8
Chaenahyun <nahyun0105@naver.com>
2023-11-13 15:03:21 +0900

    달리는 모션 추가

--- runner.py
+++ runner.py
@@ -3,8 +3,11 @@ from pico2d import *
 
 ground_width, ground_height = 800, 450
 open_canvas(ground_width, ground_height)
-ground = load_image('ground_hitting.png')
-runner = load_image('pitcher.png')
+ground = load_image('ground.png')
+running_runner_left = load_image('running_runner_left.png')
+running_runner_right = load_image('running_runner_right.png')
+#runner = load_image('idle_runner_left.png')
+#runner = load_image('idle_runner_right.png')
 
 def handle_events():
     global running
@@ -22,14 +25,15 @@ def handle_events():
 
 running = True
 frame = 0
-x, y = ground_width // 2, ground_height // 2
+x = 495  #초기 x좌표
+y = 83  #초기 y좌표
 move_arrow_x, move_arrow_y = x, y
 #hide_cursor()
 
 while running:
     clear_canvas()
     ground.draw(ground_width // 2, ground_height // 2)
-    runner.clip_draw(frame * 20, 0, 20, 20, x, y, 30, 30)
+    running_runner_left.clip_draw(frame * 45, 0, 45, 45, x, y, 30, 30)
 
     if x < move_arrow_x:
         x += 5
@@ -44,6 +48,6 @@ while running:
     update_canvas()
     handle_events()
     frame = (frame + 1) % 8
-    delay(0.05)
+    delay(0.1)
 
 close_canvas()
