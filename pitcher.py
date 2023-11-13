67f1c916879ade099733e4504292dcb36613275d
Chaenahyun <nahyun0105@naver.com>
2023-11-14 01:39:23 +0900

    스페이스바를 눌렀을 때만 투구하게 수정

--- pitcher.py
+++ pitcher.py
@@ -1,29 +1,30 @@
 from pico2d import *
 
-ground_width, ground_height =800, 450
-open_canvas(ground_width, ground_height)
-ground = load_image('ground_pitching.png')
+ground_width, ground_height = 800, 450
+SCREEN_WIDTH, SCREEN_HEIGHT = 800, 450
 
+open_canvas(ground_width, ground_height)
+
+ground = load_image('ground_pitching.png')
 hitter_left_handed = load_image('pitcher.png')
 surprised_pitcher = load_image('surprised_pitcher.png')
 fast_ball = load_image('fast_ball.png')
 
 running = True
-x = 400  # 초기 x 좌표
-y = 180  # 초기 y 좌표
+x, y = 400, 180
 frame = 0  # 변수 초기화
-dir_x = 0
-dir_y = 0
+start_pitching = False  #투구 애니메이션 재생 여부를 나타내는 변수
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 450
 
-character_width = 60
-character_height = 60
-
+def draw():
+    clear_canvas()
+    ground.draw(ground_width // 2, ground_height// 2)
+    hitter_left_handed.clip_draw(frame * 45, 0, 45, 45, x, y, 50, 50)
+    fast_ball.clip_draw(frame * 20, 0, 20, 20, x, y, 100, 100)
+    update_canvas()
 
 def handle_events():
-    global running, dir_x, dir_y, hitter_left_handed, is_idle, idle_frame  # 전역 변수
+    global running, start_pitching
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -31,21 +32,22 @@ def handle_events():
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_ESCAPE:
                 running = False
+            elif event.key == SDLK_SPACE and not start_pitching:
+                start_pitching = True
 
+def update():
+    global frame, start_pitching
+    if start_pitching:
+        frame = (frame + 1) % 8
+        delay(0.2)
+        if frame == 0:  # 애니메이션이 마지막 프레임에 도달하면 재생을 멈춤
+            start_pitching = False
+
+running = True
 
 while running:
-    clear_canvas()
-    ground.draw(ground_width//2, ground_height//2)
-    hitter_left_handed.clip_draw(frame * 45, 0, 45, 45, x, y, 50, 50)
-    fast_ball.clip_draw(frame * 20, 0, 20, 20, x, y, 100, 100)
-
-    update_canvas()
     handle_events()
-
-    frame = (frame + 1) % 8
-    delay(0.2)
-
-    if not running:
-        break
+    draw()
+    update()
 
 close_canvas()
