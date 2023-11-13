337aa479b73586edf3572bfa438a84338c9176da
Chaenahyun <nahyun0105@naver.com>
2023-11-13 14:46:34 +0900

    투구 모션 수정

--- pitcher.py
+++ pitcher.py
@@ -2,13 +2,14 @@ from pico2d import *
 
 ground_width, ground_height =800, 450
 open_canvas(ground_width, ground_height)
-ground = load_image('ground_hitting.png')
+ground = load_image('ground_pitching.png')
 
-character = load_image('pitcher.png')
+pitcher = load_image('pitcher.png')
+surprised_pitcher = load_image('surprised_pitcher.png')
 
 running = True
 x = 400  # 초기 x 좌표
-y = 160  # 초기 y 좌표
+y = 180  # 초기 y 좌표
 frame = 0  # 변수 초기화
 dir_x = 0
 dir_y = 0
@@ -21,7 +22,7 @@ character_height = 60
 
 
 def handle_events():
-    global running, dir_x, dir_y, character, is_idle, idle_frame  # 전역 변수
+    global running, dir_x, dir_y, pitcher, is_idle, idle_frame  # 전역 변수
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -34,7 +35,7 @@ def handle_events():
 while running:
     clear_canvas()
     ground.draw(ground_width//2, ground_height//2)
-    character.clip_draw(frame * 20, 0, 20, 20, x, y, 30, 30)
+    pitcher.clip_draw(frame * 45, 0, 45, 45, x, y, 50, 50)
 
     update_canvas()
     handle_events()
