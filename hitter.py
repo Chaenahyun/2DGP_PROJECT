8e5dda1c213bcd1b739c1217f7f98046b90ab1f4
Chaenahyun <nahyun0105@naver.com>
2023-11-14 00:49:48 +0900

    타자 객체 추가

--- hitter.py
+++ hitter.py
@@ -2,14 +2,14 @@ from pico2d import *
 
 ground_width, ground_height =800, 450
 open_canvas(ground_width, ground_height)
-ground = load_image('ground_pitching.png')
+ground = load_image('ground_hitting.png')
 
-hitter_left_handed = load_image('pitcher.png')
-surprised_pitcher = load_image('surprised_pitcher.png')
+hitter_left_handed = load_image('hitter_left_handed.png')
+#hitter_right_handed = load_image('hitter_right_handed.png')
 
 running = True
-x = 400  # 초기 x 좌표
-y = 180  # 초기 y 좌표
+x = 600  # 초기 x 좌표
+y = 120  # 초기 y 좌표
 frame = 0  # 변수 초기화
 dir_x = 0
 dir_y = 0
@@ -17,8 +17,8 @@ dir_y = 0
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 450
 
-character_width = 60
-character_height = 60
+character_width = 55
+character_height = 55
 
 
 def handle_events():
@@ -35,13 +35,13 @@ def handle_events():
 while running:
     clear_canvas()
     ground.draw(ground_width//2, ground_height//2)
-    hitter_left_handed.clip_draw(frame * 45, 0, 45, 45, x, y, 50, 50)
+    hitter_left_handed.clip_draw(frame * 80, 0, 80, 80, x, y, 250, 250)
 
     update_canvas()
     handle_events()
 
     frame = (frame + 1) % 8
-    delay(0.2)
+    delay(0.1)
 
     if not running:
         break
