c06ba63d7ea590490a4690531004da8e5113dea9
Chaenahyun <nahyun0105@naver.com>
2023-11-15 18:03:14 +0900

    좌클릭 시 타자 스윙, 스윙하지 않을 때는 IDLE 애니메이션 재생

--- hitter.py
+++ hitter.py
@@ -1,49 +1,74 @@
 from pico2d import *
 
-ground_width, ground_height =800, 450
+ground_width, ground_height = 800, 450
 open_canvas(ground_width, ground_height)
 ground = load_image('ground_hitting.png')
 
+# 스윙
 hitter_left_handed = load_image('hitter_left_handed.png')
 hitter_right_handed = load_image('hitter_right_handed.png')
+# idle
 idle_hitter_left_handed = load_image('idle_hitter_left_handed.png')
+idle_hitter_right_handed = load_image('idle_hitter_right_handed.png')
 
 running = True
-x = 600  # 초기 x 좌표
-y = 120  # 초기 y 좌표
-frame = 0  # 변수 초기화
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 450
+# 우타자 초기 위치
+hitter_right_handed_x = 230  # 초기 x 좌표
+hitter_right_handed_y = 150  # 초기 y 좌표
 
-character_width = 55
-character_height = 55
+hitter_frame = 0
+hitter_idle_frame = 8
+hitter_frame_count = 8
 
+hitting_delay = 0.1
+hitter_idle_delay = 0.2
+
+start_hitting = False
+start_idle = False
 
 def handle_events():
-    global running, dir_x, dir_y, hitter_left_handed, is_idle, idle_frame  # 전역 변수
+    global running, start_hitting
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
-        elif event.type == SDL_KEYDOWN:
-            if event.key == SDLK_ESCAPE:
-                running = False
+        #마우스 좌클릭 시 스윙
+        elif event.type == SDL_MOUSEBUTTONDOWN and event.button == SDL_BUTTON_LEFT:
+            start_hitting = True
 
-
-while running:
+def draw():
     clear_canvas()
-    ground.draw(ground_width//2, ground_height//2)
-    #hitter_left_handed.clip_draw(frame * 80, 0, 80, 80, x, y, 250, 250)
-    idle_hitter_left_handed.clip_draw(frame * 50, 0, 50, 50, x, y, 250, 250)
+    ground.draw(ground_width // 2, ground_height // 2)
+
+    if start_hitting:
+        hitter_right_handed.clip_draw(hitter_frame * 80, 0, 80, 80,
+            hitter_right_handed_x, hitter_right_handed_y, 490, 300)
+
+    if not start_hitting:
+        idle_hitter_right_handed.clip_draw(hitter_idle_frame * 50, 0, 50, 50,
+                    hitter_right_handed_x, hitter_right_handed_y, 300, 300)
 
     update_canvas()
+
+def update():
+    global hitter_frame, hitter_idle_frame, start_hitting, start_idle
+
+    if start_hitting:
+        hitter_frame = (hitter_frame + 1) % hitter_frame_count
+        delay(hitting_delay)
+
+        if hitter_frame == 0:
+            start_hitting = False
+
+    # 스윙하지 않을 때는 idle 애니메이션 재생
+    if not start_hitting:
+        hitter_idle_frame = (hitter_idle_frame + 1) % hitter_frame_count
+        delay(hitter_idle_delay)
+
+while running:
     handle_events()
-
-    frame = (frame + 1) % 8
-    delay(0.3)
-
-    if not running:
-        break
+    draw()
+    update()
 
 close_canvas()
--- pitcher.py
+++ pitcher.py
@@ -1,25 +1,33 @@
 from pico2d import *
 
 ground_width, ground_height = 800, 450
-SCREEN_WIDTH, SCREEN_HEIGHT = 800, 450
 
 open_canvas(ground_width, ground_height)
 ground = load_image('ground_pitching.png')
-hitter_left_handed = load_image('pitcher.png')
+strike_zoon = load_image('strike_zoon.png')
+pitcher_left_handed = load_image('pitcher.png')
 fast_ball = load_image('fast_ball.png')
 
 running = True
 x_pitcher, y_pitcher = 400, 200
 x_ball, y_ball = 380, 160
-pitcher_frame, ball_frame = 0 ,0
+pitcher_frame, ball_frame = 0, 0
+
 start_pitching = False
 pitching_fast_ball = False
 draw_fast_ball = False
 
+
+
+pitcher_frame_count = 8
+ball_frame_count = 5
+
+pitching_delay = 0.1
+
 def draw():
     clear_canvas()
     ground.draw(ground_width // 2, ground_height // 2)
-    hitter_left_handed.clip_draw(pitcher_frame * 45, 0, 45, 45, x_pitcher, y_pitcher, 100, 100)
+    pitcher_left_handed.clip_draw(pitcher_frame * 45, 0, 45, 45, x_pitcher, y_pitcher, 100, 100)
     if draw_fast_ball:
         fast_ball.clip_draw(ball_frame * 20, 0, 20, 40, x_ball, y_ball, 100, 100)
     update_canvas()
@@ -40,26 +48,28 @@ def handle_events():
 
 def update():
     global pitcher_frame, ball_frame, start_pitching, pitching_fast_ball, draw_fast_ball
+
     if start_pitching:
-        pitcher_frame = (pitcher_frame + 1) % 8
+        pitcher_frame = (pitcher_frame + 1) % pitcher_frame_count
 
         # start_pitching 애니메이션이 3번째 프레임에 도달하면 pitching_fast_ball 재생 시작
         if pitcher_frame == 3:
             pitching_fast_ball = True
 
+
         if pitching_fast_ball:
             draw_fast_ball = True
-            ball_frame = (ball_frame + 1) % 8
+            ball_frame = (ball_frame + 1) % ball_frame_count
             if ball_frame == 0:
                 pitching_fast_ball = False
                 draw_fast_ball = False
 
-        delay(0.15)
+        delay(pitching_delay)
 
         # start_pitching 애니메이션이 마지막 프레임에 도달하면 재생을 멈춤
         if pitcher_frame == 0:
             start_pitching = False
-            draw_fast_ball = False
+
 
 running = True
 
