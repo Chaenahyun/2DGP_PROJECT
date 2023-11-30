10ce9e886390e90d419abecb78b686249694ff1e
Chaenahyun <nahyun0105@naver.com>
2023-11-30 16:41:36 +0900

    볼 이미지 잘리는 것과 크기가 커지지 않는 것 수정

--- fast_ball.py
+++ fast_ball.py
@@ -6,11 +6,11 @@ class Fast_ball:
         self.ball = load_image('resource/ball.png')
         self.start_pitching = False
         self.ball_x, self.ball_y = 0, 0
-        self.speed = 5
+        self.initial_ball_x, self.initial_ball_y = 380, 200
+        self.speed = 10
         self.ball_frame = 0
         self.ball_frame_count = 8
-        self.ball_delay = 0.02
-        self.ball_size = 15
+        self.ball_size = 20
         self.target_x, self.target_y = 0, 0
 
 
@@ -30,7 +30,7 @@ class Fast_ball:
     def set_target_position(self, target):
         if not self.start_pitching:
             self.start_pitching = True
-            self.ball_x, self.ball_y = initial_ball_x, initial_ball_y
+            self.ball_x, self.ball_y = self.initial_ball_x, self.initial_ball_y
             self.ball_size = 15  # 초기 크기로 되돌림
             self.set_target(target)
 
@@ -40,7 +40,7 @@ class Fast_ball:
     def draw(self):
         if self.start_pitching:
             self.ball.draw(self.ball_x, self.ball_y, self.ball_size, self.ball_size)
-            #self.ball_size += 0.2
+            #self.ball_size += 1
 
     def update(self):
         if self.start_pitching:
@@ -57,17 +57,15 @@ class Fast_ball:
                 self.ball_x += dir_x * self.speed
                 self.ball_y += dir_y * self.speed
 
-            self.ball_size += 0.
-
-            delay(self.ball_delay)
+            self.ball_size += 0.5
 
             tolerance = 5
+
             if abs(self.ball_x - self.target_x) < tolerance and abs(self.ball_y - self.target_y) < tolerance:
                 self.start_pitching = False
                 self.ball_size = 15  # 초기 크기로 되돌림
 
-# 초기 위치
-initial_ball_x, initial_ball_y = (380, 200)
+
 
 
 # 목표 위치
