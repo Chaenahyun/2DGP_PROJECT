4c74f177027da8b4836a8abce0da62e756f63802
Chaenahyun <nahyun0105@naver.com>
2023-12-05 01:29:40 +0900

    fielder 이동

--- fielder.py
+++ fielder.py
@@ -5,9 +5,9 @@ class Fielder:
         self.idle_image = load_image('resource/idle_defender.png')
         self.running_left_image = load_image('resource/running_defender_to_left.png')
         self.running_right_image = load_image('resource/running_defender_to_right.png')
-        self.running_right_image = load_image('resource/catching_and_throwing_defender.png')
-        self.running_right_image = load_image('resource/catching_ground_ball_and_throwing_defender.png')
-        self.running_right_image = load_image('resource/jump_catching_and_throwing_defender.png')
+        # self.throwing1 = load_image('resource/catching_and_throwing_defender.png')
+        # self.throwing2 = load_image('resource/catching_ground_ball_and_throwing_defender.png')
+        # self.throwing3 = load_image('resource/jump_catching_and_throwing_defender.png')
         self.x = 400
         self.y = 160
         self.frame = 0
@@ -48,30 +48,41 @@ class Fielder:
                 elif event.key == SDLK_DOWN:
                     self.dir_y += 1
 
+    def draw(self):
+        if self.is_idle:
+            self.idle_image.clip_draw(self.frame * 45, 0, 45, 45,
+                                      self.x, self.y, 30, 30)
+        elif self.dir_x < 0:
+            self.running_right_image.clip_draw(self.frame * 45, 0, 45, 45,
+                                      self.x, self.y, 30, 30)
+        else:
+            self.running_left_image.clip_draw(self.frame * 45, 0, 45, 45,
+                                      self.x, self.y, 30, 30)
+
     def update(self):
         # x 방향 이동
         if self.dir_x > 0:
             if self.x + self.dir_x * 5 + self.character_width // 2 <= 800:
                 self.x += self.dir_x * 5
+            self.frame = (self.frame + 1) % 8
         elif self.dir_x < 0:
             if self.x + self.dir_x * 5 - self.character_width // 2 >= 0:
                 self.x += self.dir_x * 5
+            self.frame = (self.frame + 1) % 8
 
         # y 방향 이동
         if self.dir_y > 0:
             if self.y + self.dir_y * 5 + self.character_height // 2 <= 450:
                 self.y += self.dir_y * 5
+            self.frame = (self.frame + 1) % 8
         elif self.dir_y < 0:
             if self.y + self.dir_y * 5 - self.character_height // 2 >= 0:
                 self.y += self.dir_y * 5
+            self.frame = (self.frame + 1) % 8
 
-        self.frame = (self.frame + 1) % 8
-        self.idle_frame = (self.idle_frame + 1) % 8
-
-    def draw(self):
-        if self.is_idle:
-            character_image = self.character_images[0]
-            character_image.clip_draw(0, 0, 45, 45, self.x, self.y, 30, 30)
+        # 이동하지 않을 때 체크 (dir_x와 dir_y가 모두 0인 경우)
+        if self.dir_x == 0 and self.dir_y == 0:
+            self.is_idle = True
+            self.idle_frame = (self.idle_frame + 1) % 8
         else:
-            character_image = self.character_images[self.frame % 6]
-            character_image.clip_draw(0, 0, 45, 45, self.x, self.y, 30, 30)
+            self.is_idle = False
