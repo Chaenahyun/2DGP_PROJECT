b0ba84244cd4a1055903c074af79625fdee1887b
Chaenahyun <nahyun0105@naver.com>
2023-12-04 20:56:13 +0900

    게임 데이터

---
+++ lapdict.py
@@ -0,0 +1,5 @@
+data = {'x':3, 'y':2, 'size':1.5}
+data['y'] = 3
+data['size'] = 2.3
+data.update({'x':300, 'size':100})
+print(data)
---
+++ test.py
@@ -0,0 +1,12 @@
+import json
+
+number = [1, 2, 3, 4]
+
+numbers_string = json.dumps(number)
+print(type(numbers_string))
+print(numbers_string)
+
+value_string = '{"x":10, "y":20, "size":1.5}'
+value = json.loads(value_string)
+print(type(value))
+print(value)
