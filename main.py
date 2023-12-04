1c751ace81509bb2d7d75d71a02f00374f04b7b8
Chaenahyun <nahyun0105@naver.com>
2023-12-04 13:36:52 +0900

    메인 추가

---
+++ main.py
@@ -0,0 +1,11 @@
+#main.py
+
+from pico2d import (open_canvas, delay, close_canvas)
+import game_framework
+
+import play_mode as start_mode
+
+open_canvas(800, 450)
+game_framework.run(start_mode)
+close_canvas()
+
